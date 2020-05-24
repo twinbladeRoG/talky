4ee9b0fc0aaf40e55a1a9c3df52e34c56e763c36
twinbladeRoG <sohan.dutta11@gmail.com>
2020-05-24 22:40:42 +0530

    update: Auto-connect video call

--- src/app.js
+++ src/app.js
@@ -35,7 +35,8 @@ io.use((socket, next) => {
     id: uuid(),
     name,
     room,
-    socketID: socket.id
+    signal: null,
+    socketId: socket.id
   };
   socket.user = user;
   socket.room = room;
@@ -43,24 +44,34 @@ io.use((socket, next) => {
 });
 
 io.on('connection', socket => {
-  users.push(socket.user);
-  socket.join(socket.room, () => {
-    socket
-      .in(socket.room)
-      .emit('join-room', `${socket.user.name} has joined ${socket.room}`);
-    console.log(socket.rooms);
-    io.in(socket.room).emit(
+  const { user, room } = socket;
+
+  users.push(user);
+
+  socket.emit('connected', user);
+
+  socket.join(room, () => {
+    socket.in(room).emit('join-room', `${user.name} has joined ${room}`);
+
+    io.in(room).emit(
       'users',
-      users.filter(({ room }) => room === socket.room)
+      users.filter(({ room }) => room === room)
     );
   });
 
-  socket.emit('connected', socket.user);
-
-  socket.on('send-signal', res => {
-    console.log('> Signal received from: ', res.from);
-    console.log('> Signal sending to: ', res.to);
-    io.to(res.to.socketID).emit('get-signal', res);
+  socket.on('send-signal', ({ signal, from }) => {
+    console.log('> Signal received from:', from.name);
+    users = users.map(usr => {
+      if (usr.id === from.id) {
+        usr.signal = signal;
+      }
+      return usr;
+    });
+    io.in(room).emit(
+      'users',
+      users.filter(({ room: r }) => r === room)
+    );
+    // io.to(res.to.socketID).emit('get-signal', res);
   });
 
   socket.on('acknowledge-call', res => {
@@ -71,8 +82,8 @@ io.on('connection', socket => {
   socket.on('disconnect', res => {
     console.log('> Disconnect: ', socket.id);
     // console.log(res);
-    users = users.filter(user => user.socketID !== socket.id);
-    // console.log('> Users after disconnect', users);
+    users = users.filter(usr => usr.socketId !== socket.id);
+    console.log('> Users after disconnect', users.length);
     io.emit('users', users);
   });
 });
